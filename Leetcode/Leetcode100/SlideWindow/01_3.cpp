d035e07afbe1aa6ac8745866f7a847d33a91dcbd
sher <hony_sher@foxmail.com>
2024-03-13 10:44:09 +0800

    add docstring and comment using codeium

--- Leetcode/Leetcode100/SlideWindow/01_3.cpp
+++ Leetcode/Leetcode100/SlideWindow/01_3.cpp
@@ -5,15 +5,43 @@ using namespace std;
 
 class Solution {
    public:
+    /**
+     * Returns the length of the longest substring without repeating characters.
+     *
+     * @param s The input string.
+     * @return The length of the longest substring without repeating characters.
+     */
     int lengthOfLongestSubstring(string s) {
-        int n = s.size(), i = 0, j = 0, ans = 0;
+        // Initialize variables
+        int n = s.size();  // length of the input string
+        int i = 0;         // starting index of the current substring
+        int j = 0;         // ending index of the current substring
+        int ans = 0;       // length of the longest substring
+
+        // Hash map to store the index of the last occurrence of a character
         vector<int> hash(128, -1);
+
+        // Sliding window technique to find the longest substring without
+        // repeating characters
         while (j < n) {
-            if (hash[s[j]] != -1) i = max(hash[s[j]] + 1, i);
+            // If the current character is already present in the substring,
+            // move the starting index to the right of the last occurrence of
+            // the character
+            if (hash[s[j]] != -1) {
+                i = max(hash[s[j]] + 1, i);
+            }
+
+            // Update the hash map and move to the next character
             hash[s[j]] = j;
+
+            // Update the length of the longest substring
             ans = max(ans, j - i + 1);
+
+            // Move to the next character
             j++;
         }
+
+        // Return the length of the longest substring
         return ans;
     }
 };
